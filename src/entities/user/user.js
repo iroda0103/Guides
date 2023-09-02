94346f7bf7c7ce446e26d25d3c8b50c2878dd655
iroda0103 <iroda3242@gmail.com>
2023-09-02 21:44:14 +0500

    docs(users):entitiesga qo'shimcha tekshirishlarni qo'sh

--- src/entities/user/user.js
+++ src/entities/user/user.js
@@ -8,15 +8,32 @@ module.exports = function buildMakeUser({ Id, Hash }) {
     username,
     password,
   } = {}) {
-    console.log(id, first_name, last_name, role, age, username, password);
+    if (!last_name) {
+      throw new Error("Foydalanuvchida yaroqli last_name bo'lishi shart.");
+    }
+
+    if (!username) {
+      throw new Error("Foydalanuvchida yaroqli username bo'lishi shart.");
+    }
+
+    if (!password) {
+      throw new Error("Foydalanuvchida yaroqli phone bo'lishi shart.");
+    }
+
     if (!id) {
       throw new Error("Foydalanuvchida yaroqli id bo'lishi shart.");
     }
+
     if (!role) {
       throw new Error("Foydalanuvchida yaroqli rol (role) bo'lishi shart.");
     }
+
     if (!["admin", "employee"].includes(role)) {
       throw new Error("Foydalanuvchida yaroqli role bo'lishi kk");
+    }
+
+    if (!first_name) {
+      throw new Error("Foydalanuvchida yaroqli firts_name bo'lishi shart.");
     }
 
     return Object.freeze({
