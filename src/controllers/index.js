ab70ebbf20e1bd851c16d85631dda59785ca9228
iroda0103 <iroda3242@gmail.com>
2023-09-09 14:26:30 +0500

    docs:controllerga userGuide routini qo'sh

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const usersRouter = require("./users/router");
 const guideRouter = require("./guides/router");
+const userGuideRouter = require("./userGuide/router");
 
 const api = express.Router();
 
-api.use("/api", usersRouter, guideRouter);
+api.use("/api", usersRouter, guideRouter, userGuideRouter);
 
 module.exports = api;
